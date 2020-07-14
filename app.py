9e5e6f73b8cc578eaa81ee730378c0170283002a
jessicajzhang03 <jessicajzhang03@gmail.com>
2020-07-14 12:28:35 -0700

    poems

--- app.py
+++ app.py
@@ -20,3 +20,19 @@ def math_page():
 @app.route('/latex-asy',methods=['GET','POST'])
 def latex_page(): 
     return render_template('latex-asy.html')
+
+poems_list = [
+    {'title':'Le Lac', 'author': 'Lamartine','text':['yay'],'keyword':'le_lac'},
+    {'title':'First Fig','author':'Edna St. Vincent Millay',
+    'text':['My candle burns at both ends','<span class="indent">It will not last the night</span>','But ah, my foes, and oh, my friends—','<span class="indent">It gives a lovely light!</span>'],'keyword':'first_fig'}
+]
+poem_kw = {'le_lac':0,'first_fig':1}
+
+@app.route('/poems')
+def poems(): 
+    return render_template('poems.html',poems=poems_list)
+
+@app.route('/poems/<keyword>') 
+def poem(keyword): 
+    poem = poems_list[poem_kw[keyword]]
+    return render_template('poem.html',poem=poem)
